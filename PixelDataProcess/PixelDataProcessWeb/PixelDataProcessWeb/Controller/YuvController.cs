ebdf7a7b251f6b0061e951590025eb79ff780bca
beReply <2366337107@qq.com>
2021-03-18 01:56:05 +0800

    SimplestYuv420Split&&Yuv420ToGray

--- PixelDataProcess/PixelDataProcessWeb/PixelDataProcessWeb/Controller/YuvController.cs
+++ PixelDataProcess/PixelDataProcessWeb/PixelDataProcessWeb/Controller/YuvController.cs
@@ -10,17 +10,40 @@ namespace PixelDataProcessWeb.Controller
     public class YuvController : ControllerBase
     {
         [HttpPost("/SimplestYuv420Split")]
-        public async Task<string> SimplestYuv420Split(IFormFile formFile, int width, int height, int num)
+        public async Task<FileStreamResult> SimplestYuv420Split(IFormFile formFile, int width, int height)
         {
             var stream = formFile.OpenReadStream();
             var buffer = new byte[stream.Length];
-            var ss = await stream.ReadAsync(buffer, 0, (int)stream.Length);
+            var ss = await  stream.ReadAsync(buffer, 0, (int)stream.Length);
 
             var y = buffer.Skip(0).Take(width * height).ToArray();
             var u = buffer.Skip(width * height ).Take(width * height / 4 ).ToArray();
             var v = buffer.Skip(width * height * 3 / 4).Take(width * height / 4).ToArray();
+            
+            var outYStream = new MemoryStream(y);
+            var outUStream = new MemoryStream(u);
+            var outVStream = new MemoryStream(v);
+            return File(outYStream, "application/octet-stream", "output_420_y.y");
+        }
 
-            return "hello";
+        [HttpPost("/Yuv420ToGray")]
+        public async Task<FileStreamResult> Yuv420ToGray(IFormFile formFile, int width, int height)
+        {
+            var stream = formFile.OpenReadStream();
+            var buffer = new byte[stream.Length];
+            await  stream.ReadAsync(buffer, 0, (int)stream.Length);
+            
+            var y = buffer.Skip(0).Take(width * height).ToArray();
+            var append = Enumerable.Repeat((byte) 0x80, width * height / 2).ToArray();
+            var resByteArray = y.Concat(append).ToArray();
+            
+            var outStream = new MemoryStream(resByteArray);
+            return File(outStream, "application/octet-stream", "output_gray_256x256_yuv420p.yuv");
+        }
+
+        public void SimplestYuv444Split()
+        {
+            
         }
     }
 }
